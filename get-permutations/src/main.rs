a93105bce078f95b304573e351f958a6abc3406e
Owen Shepherd <owen@owen.cafe>
2021-12-07 14:39:07 +0000

    merge master

--- get-permutations/src/main.rs
+++ get-permutations/src/main.rs
@@ -1,15 +1,32 @@
-const N: i32 = 10;
 struct Perm {
     res: Vec<Vec<u8>>,
+    n: i32,
 }
+
+fn fact(n: usize) -> usize {
+    let mut res = 1;
+    for i in 2..n+1 {
+        res *= i;
+    }
+    res
+}
+
 impl Perm {
+    fn new(n: i32) -> Perm {
+
+        Perm {
+            n,
+            res: Vec::with_capacity(fact(n as usize))
+        }
+    }
+
     fn rec(&mut self, done: i32, s: Vec<u8>) {
-        if done == (1<<N)-1 {
+        if done == (1<<self.n)-1 {
             self.res.push(s);
             return;
         }
 
-        for i in 0..N {
+        for i in 0..self.n {
             if 1<<i&done > 0 {
                 continue;
             }
@@ -22,10 +39,9 @@ impl Perm {
         }
     }
 }
+
 fn main() {
-    let mut perm = Perm {
-        res: Vec::new(),
-    };
+    let mut perm = Perm::new(10);
 
     perm.rec(0, Vec::new());
     println!("Size: {}", perm.res.len());
